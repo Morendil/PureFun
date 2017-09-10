2cac09d1cab88d765fa5f9f7ed62e744f216ea1b
Laurent Bossavit <laurent.git@bossavit.com>
2017-09-10 17:13:12 +0200

    Rip out signals, add random motion

--- src/Main.purs
+++ src/Main.purs
@@ -7,47 +7,44 @@ import Data.Maybe (Maybe(..))
 import Graphics.Canvas (Arc, CANVAS, Context2D, arc, fillPath, fillRect, getCanvasElementById,
 						getCanvasHeight, getCanvasWidth, getContext2D, setFillStyle)
 
--- Things that we respond to, updating our state
-import Signal (foldp, runSignal)
-import Signal.DOM (animationFrame)
-import Signal.Time (Time)
-import Math (sin, cos)
-
 -- Things that we'd rather not deal with
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Timer (TIMER)
+import Control.Monad.Eff.Random (RANDOM, random)
+import Control.Monad.Eff.Ref (REF, newRef, readRef, writeRef, Ref)
 import DOM (DOM)
+import DOM.HTML (window)
+import DOM.HTML.Types (Window)
+import DOM.HTML.Window (requestAnimationFrame)
 
 type World =
   { x :: Number
   , y :: Number
-  , start :: Time
   }
 
-update :: Time -> World -> World
-update time {x, y, start: 0.0} = {x: x, y: y, start: time}
-update time {x, y, start: start} = {x: sin(time/100.0)*(time-start)/1000.0, y: cos(time/100.0)*(time-start)/1000.0, start: start}
+type Move =
+  { dx :: Number
+  , dy :: Number
+  }
+
+randomMove :: forall e. Eff (random :: RANDOM | e) Move
+randomMove = do
+	dx <- random
+	dy <- random
+	pure {dx: dx, dy: dy}
 
 initialState :: World
-initialState = {x: 0.0, y: 0.0, start: 0.0}
+initialState = {x: 0.0, y: 0.0}
 
-main :: forall e. Eff (canvas :: CANVAS, dom :: DOM, timer :: TIMER | e) Unit
-main = do
-  mcanvas <- getCanvasElementById "canvas"
-  case mcanvas of
-    Just canvas -> do
-      width <- getCanvasWidth canvas
-      height <- getCanvasHeight canvas
-      ctx <- getContext2D canvas
-      frames <- animationFrame
-      -- foldp :: forall a b. (a -> b -> b) -> b -> (Signal a) -> (Signal b)
-      -- animate :: Signal World
-      let animate = foldp update initialState frames
-      -- map :: forall a b. (a -> b) -> f a -> f b
-      -- (World -> Eff _ Unit) -> Signal World -> Signal (Eff _ Unit)
-      -- runSignal :: forall e. Signal (Eff e Unit) -> Eff e Unit
-      runSignal (view ctx width height <$> animate)
-    Nothing -> pure unit
+main :: Eff _ Unit
+main = withAnimateContext "canvas" initialState animate
+
+animate ctx width height state = do
+  move <- randomMove
+  _ <- view ctx width height state
+
+  -- Update the state
+  pure $ state { x = state.x + move.dx - 0.5
+               , y = state.y + move.dy - 0.5 }
 
 circle :: World -> Arc
 circle state =
@@ -65,3 +62,42 @@ view ctx width height state = do
   _ <- setFillStyle "#0000FF" ctx
   _ <- fillPath ctx $ arc ctx $ circle state
   pure unit
+
+loopAnimation :: forall e state.
+  Window ->
+  Ref state ->
+  state ->
+  (state -> Eff _ state) ->
+  Eff _ Unit
+loopAnimation window ref state step =
+  void $ requestAnimationFrame
+    do loopAnimation window ref state step
+       state1 <- readRef ref
+       state2 <- step state1
+       writeRef ref state2
+    window
+
+withAnimation :: forall e state.
+  state ->
+  (state -> Eff _ state) ->
+  Eff _ Unit
+withAnimation state step = do
+  window <- window
+  ref <- newRef state
+  loopAnimation window ref state step
+
+
+withAnimateContext :: forall e state.
+  String ->
+  state ->
+  (Context2D -> Number -> Number -> state -> Eff _ state) ->
+  Eff _ Unit
+withAnimateContext name state draw = do
+  canvas <- getCanvasElementById name
+  case canvas of
+    Just canvas1 -> do
+      width <- getCanvasWidth canvas1
+      height <- getCanvasHeight canvas1
+      ctx <- getContext2D canvas1
+      withAnimation state $ draw ctx width height
+    Nothing -> pure unit

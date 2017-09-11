91ec58d97ef8132b9b5a44097500b9735878d2b9
Laurent Bossavit <laurent.git@bossavit.com>
2017-09-12 00:08:17 +0200

    Mix randomness into signals, somehow

--- src/Main.purs
+++ src/Main.purs
@@ -1,67 +1,22 @@
 module Main where
 
 import Prelude
-import Data.Maybe (Maybe(..))
-
--- Drawing stuff to the screen
-import Graphics.Canvas (Arc, CANVAS, Context2D, arc, fillPath, fillRect, getCanvasElementById,
-						getCanvasHeight, getCanvasWidth, getContext2D, setFillStyle)
-
--- Things that we respond to, updating our state
-import Signal (foldp, runSignal)
-import Signal.DOM (animationFrame)
-import Signal.Time (Time)
-import Math (sin, cos)
-
--- Things that we'd rather not deal with
+import Control.Apply ((*>))
+import Signal (Signal(), unwrap)
+import Signal.Time (every, second)
+import Flare (UI(), button, foldp, runFlareShow, lift)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Timer (TIMER)
-import DOM (DOM)
+import Control.Monad.Eff.Random
+import Control.Apply (lift2)
 
-type World =
-  { x :: Number
-  , y :: Number
-  , start :: Time
-  }
+tick :: Signal (Eff _ Int)
+tick = every second *> pure (randomInt 1 10)
 
-update :: Time -> World -> World
-update time {x, y, start: 0.0} = {x: x, y: y, start: time}
-update time {x, y, start: start} = {x: sin(time/100.0)*(time-start)/1000.0, y: cos(time/100.0)*(time-start)/1000.0, start: start}
+-- foldp :: forall a b e. (a -> b -> b) -> b -> UI e a -> UI e b
 
-initialState :: World
-initialState = {x: 0.0, y: 0.0, start: 0.0}
+score :: UI _ Int
+score = foldp (+) 0 $ lift2 (+) (lift $ unwrap tick) (button "Click me!" 0 1)
 
-main :: forall e. Eff (canvas :: CANVAS, dom :: DOM, timer :: TIMER | e) Unit
-main = do
-  mcanvas <- getCanvasElementById "canvas"
-  case mcanvas of
-    Just canvas -> do
-      width <- getCanvasWidth canvas
-      height <- getCanvasHeight canvas
-      ctx <- getContext2D canvas
-      frames <- animationFrame
-      -- foldp :: forall a b. (a -> b -> b) -> b -> (Signal a) -> (Signal b)
-      -- animate :: Signal World
-      let animate = foldp update initialState frames
-      -- map :: forall a b. (a -> b) -> f a -> f b
-      -- (World -> Eff _ Unit) -> Signal World -> Signal (Eff _ Unit)
-      -- runSignal :: forall e. Signal (Eff e Unit) -> Eff e Unit
-      runSignal (view ctx width height <$> animate)
-    Nothing -> pure unit
+-- runFlareShow :: forall e a. Show a => ElementId -> ElementId -> UI e a -> Eff (dom :: DOM, channel :: CHANNEL | e) Unit
 
-circle :: World -> Arc
-circle state =
-        { x:  state.x + (640.0 / 2.0)
-        , y:  state.y + (480.0 / 2.0)
-        , r: 10.0
-        , start: 0.0
-        , end: 5.0
-        }
-
-view :: forall e. Context2D -> Number -> Number -> World -> Eff (canvas :: CANVAS | e) Unit
-view ctx width height state = do
-  _ <- setFillStyle "#FFFFFF" ctx
-  _ <- fillRect ctx { x: 0.0, y: 0.0, w: width, h: height }
-  _ <- setFillStyle "#0000FF" ctx
-  _ <- fillPath ctx $ arc ctx $ circle state
-  pure unit
+main = runFlareShow "controls" "output" score
